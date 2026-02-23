f3392cdeb34296deeccd25eef914c2c8ba932023
Amritraj96 <amritraj.up.yadav@gmail.com>
2026-02-23 20:35:57 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,39 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { fetchData } from "../redux/actions"; // Adjust path if necessary
+import '../styles/App.css';
 
 const App = () => {
+  const dispatch = useDispatch();
+  const { loading, data, error } = useSelector((state) => state);
+
+  useEffect(() => {
+    dispatch(fetchData());
+  }, [dispatch]);
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      {/* Do not remove the main div */}
+      <div className="header">
+        <h1>A short Naration of Lorem Ipsum</h1>
+        <p>Below Contains A title and Body gotten from<br/>a random API, Please take your time to Review</p>
+      </div>
+
+      {/* Loading State Implementation */}
+      {loading && <p>Loading...</p>}
+      {error && <p>Error: {error}</p>}
+
+      {/* Data Display */}
+      <div className="grid-container">
+        {!loading && data && data.map((item, index) => (
+          <div key={item.id || index} className="card">
+            <p><strong>Title :</strong>{item.title}</p>
+            <p><strong>Body :</strong>{item.body}</p>
+          </div>
+        ))}
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
