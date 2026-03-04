d7b5ed1fea8eb9fb7fa14d0a6c052c0548657617
Amritraj96 <amritraj.up.yadav@gmail.com>
2026-03-04 19:34:47 +0530

    Update actions.js

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,17 +1,33 @@
+// Action Types
 export const FETCH_DATA_REQUEST = 'FETCH_DATA_REQUEST';
 export const FETCH_DATA_SUCCESS = 'FETCH_DATA_SUCCESS';
 export const FETCH_DATA_FAILURE = 'FETCH_DATA_FAILURE';
 
-export const fetchData = () => {
+// Action Creators
+export const fetchDataRequest = () => ({
+  type: FETCH_DATA_REQUEST,
+});
+
+export const fetchDataSuccess = (data) => ({
+  type: FETCH_DATA_SUCCESS,
+  payload: data,
+});
+
+export const fetchDataFailure = (error) => ({
+  type: FETCH_DATA_FAILURE,
+  payload: error,
+});
+
+// Thunk Function to fetch API data
+export const fetchLoremData = () => {
   return async (dispatch) => {
-    dispatch({ type: FETCH_DATA_REQUEST });
+    dispatch(fetchDataRequest());
     try {
-      // Fetching from the specified API
       const response = await fetch('https://api.lorem.com/ipsum');
       const data = await response.json();
-      dispatch({ type: FETCH_DATA_SUCCESS, payload: data });
+      dispatch(fetchDataSuccess(data));
     } catch (error) {
-      dispatch({ type: FETCH_DATA_FAILURE, payload: error.message });
+      dispatch(fetchDataFailure(error.message));
     }
   };
 };
